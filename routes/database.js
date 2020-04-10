86e6e10a18fe2ce29868d9d9a17216336ab3dffe
parkjp <parkjp@anipen.com>
2020-04-11 04:26:16 +0900

    update

--- routes/database.js
+++ routes/database.js
@@ -1,9 +1,11 @@
 const mysql      = require('mysql');
 const dbConfig = {
     host     : '34.64.153.190',
+    port     : '3306',
     user     : 'root',
     password : '1q2w3e4r!!@@',
-    database : 'localCurrency'
+    database : 'localCurrency',
+    socketPath: '/cloudsql/mask-270506:asia-northeast3:main-sql'
 };
 const pool = mysql.createPool(dbConfig);
 
